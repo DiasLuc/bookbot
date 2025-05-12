17abe86313fbd8b4c948fd7567bbf338ad2be2a4
DiasLuc <diaslucp@gmail.com>
2025-05-12 15:48:46 -0400

    reworked the report, and put counting functions in stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, character_count
+from stats import get_num_words, character_count, chars_to_list, sort_list
 import sys
 def main():
     if len(sys.argv) != 2:
@@ -11,7 +11,7 @@ def main():
     chars_list = chars_to_list(chars_dict)
     sorted_list = sort_list(chars_list)
 
-    report = generate_report(num_words, sorted_list)
+    report = generate_report(book_path, num_words, sorted_list)
 
     print(report)
     
@@ -23,34 +23,14 @@ def get_book_text(path):
         return f.read()   
 
 
-
-
-
-
-
-def chars_to_list(chars):
-    list = []
-    for char in chars:
-        if char.isalpha():
-            list.append({"letter":char,"count":chars[char]})
-    return list
-
-def sort_list(list):
-    def sort_on(dict):
-        return dict["count"]
-    
-    list.sort(reverse=True, key=sort_on)
-    return list
-
-
-
-def generate_report(word_count, char_counts):
+def generate_report(book_path, word_count, char_counts):
         
-        print(f"--- Begin report of books/frankenstein.txt ---")
-        print(f"{word_count} words found in the document\n")
+        print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}...")
+        print(f"----------- Word Count ----------\nFound {word_count} total words")
+        print("--------- Character Count -------")
         for char in char_counts:
-            print(f"The '{char["letter"]}' character was found {char["count"]} times")
-        print(f"--- End report ---")
+            print(f"{char["char"]}: {char["num"]}")
+        print(f"============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -10,3 +10,17 @@ def character_count(text):
         else:
             char_counts[char] = 1
     return char_counts
+
+def chars_to_list(chars):
+    list = []
+    for char in chars:
+        if char.isalpha():
+            list.append({"char":char,"num":chars[char]})
+    return list
+
+def sort_list(list):
+    def sort_on(dict):
+        return dict["num"]
+    
+    list.sort(reverse=True, key=sort_on)
+    return list
